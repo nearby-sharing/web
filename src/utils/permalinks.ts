1a77f8b6d132c03ff32faf56e5d00899be4f73db
Lukas Kurz <lukas@shortdev.de>
2023-06-14 16:46:57 +0200

    Fix astro build

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -1,6 +1,6 @@
 import slugify from 'limax';
 
-import { SITE, BLOG } from '~/config.mjs';
+import { SITE } from '~/config.mjs';
 import { trim } from '~/utils/utils';
 
 export const trimSlash = (s: string) => trim(trim(s, '/'));
@@ -20,55 +20,27 @@ export const cleanSlug = (text = '') =>
     .map((slug) => slugify(slug))
     .join('/');
 
-export const POST_PERMALINK_PATTERN = trimSlash(BLOG?.post?.permalink || '/%slug%');
-
-export const BLOG_BASE = cleanSlug(BLOG?.list?.pathname);
-export const CATEGORY_BASE = cleanSlug(BLOG?.category?.pathname || 'category');
-export const TAG_BASE = cleanSlug(BLOG?.tag?.pathname) || 'tag';
-
 /** */
 export const getCanonical = (path = ''): string | URL => {
   const url = String(new URL(path, SITE.origin));
   if (SITE.trailingSlash == false && path && url.endsWith('/')) {
-    return url.slice(0,-1)
+    return url.slice(0, -1)
   }
-  else if (SITE.trailingSlash == true && path && !url.endsWith('/') ) {
+  else if (SITE.trailingSlash == true && path && !url.endsWith('/')) {
     return url + '/';
   }
   return url;
 }
 
 /** */
-export const getPermalink = (slug = '', type = 'page'): string => {
-  let permalink: string;
-
-  switch (type) {
-    case 'category':
-      permalink = createPath(CATEGORY_BASE, trimSlash(slug));
-      break;
-
-    case 'tag':
-      permalink = createPath(TAG_BASE, trimSlash(slug));
-      break;
-
-    case 'post':
-      permalink = createPath(trimSlash(slug));
-      break;
-
-    case 'page':
-    default:
-      permalink = createPath(slug);
-      break;
-  }
+export const getPermalink = (slug = ''): string => {
+  let permalink = createPath(slug);
 
   return definitivePermalink(permalink);
 };
 
 /** */
 export const getHomePermalink = (): string => getPermalink('/');
-
-/** */
-export const getBlogPermalink = (): string => getPermalink(BLOG_BASE);
 
 /** */
 export const getAsset = (path: string): string =>
